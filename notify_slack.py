a12191ed09ccf85aecc54a26adf762c4df19ce6f
arai_kazuki <zukain5@gmail.com>
2021-09-22 00:04:31 +0900

    add notify_slack

--- notify_slack.py
+++ notify_slack.py
@@ -1,7 +1,14 @@
 import slackweb
+from orders import sum_of_earnings
 from secret import SLACK_URL
 
 
 def notify_slack(orders):
+    if len(orders) == 0:
+        return
+
     slack = slackweb.Slack(url=SLACK_URL)
-    
+    text = f'昨日の売上は{sum_of_earnings(orders)}円！'
+    slack.notify(text=text)
+
+    return
