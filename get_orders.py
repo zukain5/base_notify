bbeba1b92106bf40cc8a1cbba2aa8f5710bbe5d4
arai_kazuki <zukain5@gmail.com>
2021-09-21 23:29:43 +0900

    add get_orders

---
+++ get_orders.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+
+def dt_to_str(dt):
+    return dt.strftime('%Y-%m-%d %H:%M:%S')
+
+
+def get_orders(start_ordered, end_ordered, access_token):
+    req_path = 'https://api.thebase.in/1/orders'
+
+    params = {
+        'start_ordered': dt_to_str(start_ordered),
+        'end_ordered': dt_to_str(end_ordered),
+        'limit': 100,
+        'offset': 0,
+    }
+
+    header = {
+        "Authorization": "Bearer " + access_token,
+    }
+
+    res = requests.get(req_path, params=params, headers=header)
+    orders_data = json.loads(res.text)
+
+    return orders_data['orders']
